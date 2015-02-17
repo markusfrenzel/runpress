b90f42829e4916a89b08cec81bd7995bbe658b38
Markus Frenzel <github@markusfrenzel.de>
2015-02-17 16:45:03 +0100

    New Widget possibilities
    Added some variations to the widget. Still BETA.

--- inc/widget/runpress-widget.php
+++ inc/widget/runpress-widget.php
@@ -59,24 +59,44 @@ class runpress_widget extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 
+		global $wpdb;
+		global $runpress_db_name;
+		
 		$title = apply_filters( 'widget_title', $instance['title'] );
 
+		$l = !empty( $instance['lasttrack'] ) ? '1' : '0';
+		$o = !empty( $instance['onlyhighscores'] ) ? '1' : '0';
+		$s = !empty( $instance['showtable'] ) ? '1' : '0';
+
 		echo $args['before_widget'];
-		if ( ! empty( $title ) )
+		if( ! empty( $title ) )
 			echo $args['before_title'] . $title . $args['after_title'];
 
+		if($l) {
 			/* Select the last activity from the db and post its data into the widget */
-			/* this is only a test to implement my ideas into the widget area */
-			global $wpdb;
-			global $runpress_db_name;
-
-		$query = $wpdb->get_results( "SELECT * FROM $runpress_db_name ORDER BY id desc LIMIT 1", OBJECT );
+			$query = $wpdb->get_results( "SELECT * FROM $runpress_db_name ORDER BY id desc LIMIT 1", OBJECT );
 		
-		foreach( $query as $row ) {
-			echo "My latest running activity was " . $row->feeling;
-			echo "<br><img src='http:" . str_replace( 'width=50&height=70', 'width=200&height=280', $row->map_url ) . "'>";
-			echo "<br>ID: ". $row->id;
+			foreach( $query as $row ) {
+				echo "My latest running activity was " . $row->feeling;
+				echo "<br><img src='http:" . str_replace( 'width=50&height=70', 'width=200&height=280', $row->map_url ) . "'>";
+				echo "<br>ID: ". $row->id;
+			}
 		}
+		
+		if($o) {
+			/* Select only the highscore values */
+			$query = $wpdb->get_results( "SELECT pace FROM $runpress_db_name WHERE pace>0 ORDER BY pace asc LIMIT 1", OBJECT );
+			
+			foreach( $query as $row ) {
+				echo "Pace: " . $row->pace . "<br />";
+			}
+			$query = $wpdb->get_results( "SELECT duration FROM $runpress_db_name ORDER BY duration DESC LIMIT 1", OBJECT );
+			
+			foreach( $query as $row ) {
+				echo "Duration: " . $row->duration . "<br />";
+			} 
+		}
+		
 		echo $args['after_widget'];
 	}
 
@@ -88,17 +108,31 @@ class runpress_widget extends WP_Widget {
 	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
+		$instance = wp_parse_args( ( array ) $instance, array( 'title' => '' ) );
 		if ( isset( $instance[ 'title' ] ) ) {
 			$title = $instance[ 'title' ];
 		}
 		else {
-			$title = __( 'New title', 'runpress' );
+			$title = __( 'RunPress Widget', 'runpress' );
 		}
+		$lasttrack = isset( $instance[ 'lasttrack' ] ) ? (bool) $instance[ 'lasttrack' ] : false;
+		$onlyhighscores = isset( $instance[ 'onlyhighscores' ] ) ? (bool) $instance[ 'onlyhighscores'] : false;
+		$showtable = isset( $instance[ 'showtable' ] ) ? (booL) $instance[ 'showtable' ] : false;
 		?>
-		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' , 'runpress'); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>"></p>
+
+		<p><input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'lasttrack' ); ?>" name="<?php echo $this->get_field_name( 'lasttrack' ); ?>"<?php checked( $lasttrack ); ?> />
+		<label for="<?php echo $this->get_field_id( 'lasttrack' ); ?>"><?php _e( 'Show last activity', 'runpress' ); ?></label><br />
+
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'onlyhighscores' ); ?>" name="<?php echo $this->get_field_name( 'onlyhighscores'); ?>"<?php checked( $onlyhighscores ); ?> />
+		<label for="<?php echo $this->get_field_id( 'onlyhighscores' ); ?>"><?php _e( 'Show highscores', 'runpress' ); ?></label><br />
+		
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'showtable' ); ?>" name="<?php echo $this->get_field_name( 'showtable' ); ?>"<?php checked( $showtable ); ?> />
+		<label for="<?php echo $this->get_field_id( 'showtable' ); ?>"><?php _e( 'Show last 5 entries', 'runpress' ); ?></label><br />
+		
 		</p>
+
 		<?php 
 	}
 
@@ -115,6 +149,9 @@ class runpress_widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance[ 'lasttrack' ] = !empty( $new_instance[ 'lasttrack' ] ) ? 1 : 0;
+		$instance[ 'onlyhighscores' ] = !empty( $new_instance[ 'onlyhighscores' ] ) ? 1 : 0;
+		$instance[ 'showtable' ] = !empty( $new_instance[ 'showtable' ] ) ? 1 : 0;
 
 		return $instance;
 	}
